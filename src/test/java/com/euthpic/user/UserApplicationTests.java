9203b31c1203e9d758278b043cc83359e363896c
Euthpic <490114771@qq.com>
2020-02-24 23:46:01 +0800

    initial

---
+++ src/test/java/com/euthpic/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euthpic.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
